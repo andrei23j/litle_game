80b71f77096937c5d6c50b3b036ad61559da7762
andrei23j <poscuda777@gmail.com>
2021-03-09 16:28:00 +0300

    First initialization the main programm file

---
+++ atm_game/atm.py
@@ -0,0 +1,14 @@
+# Importing additional libraries and files
+import random
+import math
+import time
+import my_bank_class
+import dude_class
+import additional_dll
+
+a = dude_class.Dude()
+print(a.total_money)
+print(a.give_money())
+print(a.wallet)
+print(a.total_money)
+
